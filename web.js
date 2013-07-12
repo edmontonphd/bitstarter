5e0cb46a1108e2758e47e11996ffb9cde8f1ddee
Muraari Vasudevan <muraariv@gmail.com>
2013-07-12 05:29:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
 var express = require('express');
 
+var fs=require('fs');
+
+var buf=new Buffer(32000);
+
+buf= fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+var xx=buf.toString();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(xx);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
